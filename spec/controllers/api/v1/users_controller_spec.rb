f8e6d9bd2228217f4533caf296eb83fe20ce31f1
Sambath Prum <psb.sambath@gmail.com>
2015-02-20 12:32:08 -0600

    add destroy action to the users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -90,4 +90,13 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     end
   end
 
+  describe 'DELETE #destroy' do
+    before :each do
+      @user = create(:user)
+      delete :destroy, { id: @user.id }, format: :json
+    end
+
+    it { should respond_with 204 }
+  end
+
 end
